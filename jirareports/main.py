1ff9287160d38041e283abdc55c346302e544ad4
ownport <ownport@gmail.com>
2016-11-24 11:48:40 +0200

    [+] logging, dump and reports parameters are optional

--- jirareports/main.py
+++ jirareports/main.py
@@ -3,6 +3,17 @@ import os
 import sys
 import argparse
 
+import logging
+# Set default logging handler to avoid "No handler found" warnings.
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logger = logging.getLogger(__name__)
+
 from __init__ import __title__
 from __init__ import __version__
 
@@ -17,14 +28,28 @@ def run():
     parser.add_argument('-p', '--profile', required=True,
             action='append', help='the path to the package profile, yaml file')
     parser.add_argument('-a', '--alias', help='the JQL alias in the profile')
-    parser.add_argument('-d', '--dump', action='store_false', help='dump issues to file storages')
-    parser.add_argument('-r', '--report', action='store_false', help='generate reports')
+    parser.add_argument('-d', '--dump', action='store_true', help='dump issues to file storage')
+    parser.add_argument('-r', '--report', action='store_true', help='generate reports')
+    parser.add_argument('-l', '--logging', type=str,
+            help='logging level: DEBUG, INFO, WARNING, ERROR, CRITICAL')
     args = parser.parse_args()
+
+    log_level = logging.ERROR
+    if args.logging:
+        log_level = getattr(logging, args.logging.upper(), None)
+        if not isinstance(log_level, int):
+            logger.warning('Invalid log level: %s' % log_level)
+            logger.info('Default log level: ERROR')
+
+    logging.basicConfig(level=log_level,
+                        handler=NullHandler(),
+                        format="%(asctime)s (%(name)s) [%(levelname)s] %(message)s"
+    )
 
     if args.profile:
         for profile_path in args.profile:
             if not os.path.exists(profile_path):
-                print >> sys.stderr, '[ERROR] The path to profile does not exist, %s' % profile
+                logger.error('The path to profile does not exist, %s' % profile_path)
                 sys.exit(1)
             process(profile_path, args.alias, **{'dump': args.dump, 'report': args.report})
     else:
@@ -35,7 +60,8 @@ def process(profile_path, alias, **opts):
 
     _profile = Profile(profile_path)
     if not alias and 'jira.jqls.default' not in _profile.jql():
-        raise RuntimeError('The alias is not specified')
+        logger.error('The alias is not specified')
+        sys.exit(1)
 
     JQL = None
     if alias:
@@ -44,7 +70,7 @@ def process(profile_path, alias, **opts):
         raise RuntimeError('JQL is not specified')
 
     if 'dump' in opts and opts['dump']:
-        print >> sys.stderr, '[INFO] Dumping issues into the storage: %s' % opts['dump']
+        logger.info('Dumping issues into the storage: %s' % opts['dump'])
         dump(JQL, _profile.hostname, _profile.username, _profile.password, _profile.storage)
 
 
@@ -57,5 +83,3 @@ def dump(JQL, hostname, username, password, storage_path):
     for issue in _api.search(JQL):
         sys.stdout.write('.')
         _storage.put(issue['issue'], **issue)
-    print
-    
