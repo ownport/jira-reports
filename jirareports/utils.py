3a2f45be69afb17e668d0320bb606894e7371c9f
ownport <ownport@gmail.com>
2016-11-28 07:36:56 +0200

    [+] dict2flat()

--- jirareports/utils.py
+++ jirareports/utils.py
@@ -1,4 +1,5 @@
 
+import re
 import importlib
 
 def import_module(name, package=None):
@@ -21,3 +22,39 @@ def package_exists(name):
         return False
     else:
         return True
+
+
+def dict2flat(root_name, source, removeEmptyFields=False):
+    ''' returns a simplified "flat" form of the complex hierarchical dictionary
+    '''
+
+    def is_simple_elements(source):
+        ''' check if the source contains simple element types,
+        not lists, tuples, dicts
+        '''
+        for i in source:
+            if isinstance(i, (list, tuple, dict)):
+                return False
+        return True
+
+    flat_dict = {}
+    if isinstance(source, (list, tuple)):
+        if not is_simple_elements(source):
+            for i,e in enumerate(source):
+                new_root_name = "%s[%d]" % (root_name,i)
+                for k,v in dict2flat(new_root_name,e).items():
+                    flat_dict[k] = v
+        else:
+            flat_dict[root_name] = source
+    elif isinstance(source, dict):
+        for k,v in source.items():
+            if root_name:
+                new_root_name = "%s.%s" % (root_name, k)
+            else:
+                new_root_name = "%s" % k
+            for kk, vv in dict2flat(new_root_name,v).items():
+                flat_dict[kk] = vv
+    else:
+        if source is not None:
+            flat_dict[root_name] = source
+    return flat_dict
