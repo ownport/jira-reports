6086953dd3fee5861d60b8ad9f7410ef4b9c83fa
ownport <ownport@gmail.com>
2016-11-28 07:27:06 +0200

    [+] storage name & API changes

--- jirareports/storage.py
+++ jirareports/storage.py
@@ -6,18 +6,28 @@ sqlitedict = utils.import_module('sqlitedict', package='jirareports')
 
 class Storage(object):
 
-    def __init__(self, path):
+    def __init__(self, name, path):
 
-        self._storage = sqlitedict.SqliteDict(path, autocommit=True)
+        if not name:
+            raise RuntimeError('Storage name does not specified, %s' % name)
+        self._storage = sqlitedict.SqliteDict(path, tablename=name, autocommit=True)
 
 
-    def put(self, issue_id, **details):
+    def put(self, issue_id, details):
         ''' put issue into storage
         '''
         self._storage[issue_id] = details
 
 
-    def get(self, issue_id):
+    def get(self, issue_id=None):
         ''' get issue details by the issue id
         '''
-        return self._storage[issue_id]
+        if not issue_id:
+            return self._storage.items()
+        else:
+            return self._storage[issue_id]
+
+    def close(self):
+        ''' close storage
+        '''
+        self._storage.close()
