c3574d2bcad5c90512150a4181cf902588c29219
ownport <ownport@gmail.com>
2016-11-28 07:28:30 +0200

    [+] storage name & API changes

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,13 +8,13 @@ from jirareports.storage import Storage
 def test_storage_create(tmpdir):
 
     _path = str(tmpdir.join('storage.sqlite'))
-    _storage = Storage(_path)
+    _storage = Storage('test_storage_create', _path)
     assert isinstance(_storage, Storage)
 
 
 def test_storage_put_and_get(tmpdir):
 
     _path = str(tmpdir.join('storage.sqlite'))
-    _storage = Storage(_path)
-    _storage.put('1', **{'k1':'v1', 'k2':'v2'})
+    _storage = Storage('test_storage_put_and_get', _path)
+    _storage.put('1', {'k1':'v1', 'k2':'v2'})
     assert _storage.get('1') == {'k1':'v1', 'k2':'v2'}
