97b52241e4f958de41e80f52fbafec7307ac12ec
ownport <ownport@gmail.com>
2016-12-09 07:27:23 +0200

    [+] rounding time

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -10,24 +10,24 @@ def test_dt_round_value():
         metrics.dt(DATETIME), metrics.dt_round(DATETIME, 900),
         metrics.dt_round(DATETIME, 3600), metrics.dt_round(DATETIME, 86400),
     ) == (
-        '2016-01-11 22:55:20', '2016-01-11 22:45:00',
-        '2016-01-11 22:00:00', '2016-01-11 00:00:00',
+        '2016-01-11 22:55:20', '2016-01-11 22:45',
+        '2016-01-11 22', '2016-01-11',
     )
     DATETIME = 1452063983
     assert (
         metrics.dt(DATETIME), metrics.dt_round(DATETIME, 900),
         metrics.dt_round(DATETIME, 3600), metrics.dt_round(DATETIME, 86400),
     ) == (
-        '2016-01-06 07:06:23', '2016-01-06 07:00:00',
-        '2016-01-06 07:00:00', '2016-01-06 00:00:00',
+        '2016-01-06 07:06:23', '2016-01-06 07:00',
+        '2016-01-06 07', '2016-01-06',
     )
     DATETIME = 1458832665
     assert (
         metrics.dt(DATETIME), metrics.dt_round(DATETIME, 900),
         metrics.dt_round(DATETIME, 3600), metrics.dt_round(DATETIME, 86400),
     ) == (
-        '2016-03-24 15:17:45', '2016-03-24 15:15:00',
-        '2016-03-24 15:00:00', '2016-03-24 00:00:00',
+        '2016-03-24 15:17:45', '2016-03-24 15:15',
+        '2016-03-24 15', '2016-03-24',
     )
 
 def test_dt_round_interval():
